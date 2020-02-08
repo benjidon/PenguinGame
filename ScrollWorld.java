046ca7b7292f85327fd79ccef17926e2411d36a2
benjidon466 <bddon466@gmail.com>
2020-02-07 18:36:24 -0800

    Add music!

--- ScrollWorld.java
+++ ScrollWorld.java
@@ -11,6 +11,9 @@ public class ScrollWorld extends World
     private int cameraOffsetX, cameraOffsetY;
     private int worldX, worldY, worldWidth, worldHeight;
     private int dx = 0;
+    
+    static GreenfootSound music  = new GreenfootSound("overworld.mp3");
+    private boolean playing = false;
         
     private GreenfootImage background = null;
     private GreenfootImage treesBg = null;
@@ -27,13 +30,18 @@ public class ScrollWorld extends World
         worldWidth = getWidth();
         worldHeight = getHeight();
         
+        // Initialize Background Images
         GreenfootImage snowBg = new GreenfootImage("Background_101.png");
         treesBg = new GreenfootImage("Background_38.png");
-        
         setScrollingBackground(snowBg);
-        //Background forest = new Background();
-        //addObject(forest, 300, 300);
         
+        // Start Music Loop
+        if(!music.isPlaying() && playing == false)
+        {
+            music.playLoop();
+            playing = true;
+            music.setVolume(65);
+        }
         
         Slippy slippy = new Slippy();
         addObject(slippy, 480, 430);
@@ -41,7 +49,6 @@ public class ScrollWorld extends World
     
     private void setScrollingBackground(GreenfootImage image) {
         background = new GreenfootImage(image);
-        //background.scale(1500, 600);
         setBackground(background);
         scrollBackground(1);
     }
@@ -92,8 +99,8 @@ public class ScrollWorld extends World
     public void scrollBackground(int direction) {
         setBackground("starrynight.png");
         getBackground().drawImage(background, (dx / 50), 0);
-        getBackground().drawImage(treesBg, (dx / 2), 250);
-        getBackground().drawImage(treesBg, (dx + 2000) / 2, 250);
+        getBackground().drawImage(treesBg, (dx / 2), 220);
+        getBackground().drawImage(treesBg, (dx + 2000) / 2, 220);
         dx += direction;
    
     }
