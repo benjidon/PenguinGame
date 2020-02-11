3a9cb1572063b5caa9556b335576eb24cc431fce
James Asbury <jamesasbury03@gmail.com>
2020-02-11 15:02:34 -0800

    flight slippy movement

--- FlightSlippy.java
+++ FlightSlippy.java
@@ -8,15 +8,101 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class FlightSlippy extends Character
 {
-    /**
-     * Act - do whatever the FlightSlippy wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    private boolean facingForwards = false;
+    private float xVelocity = 0;
+    private float maxXVelocity = 5;
+    private float initYVelocity = 0;
+    private float yVelocity = 0;
+    private float jumpSpeed = 0.5f;
+    private double timeStep = 10;
+    private boolean onGround = true;
+    private boolean holding = false;
+    
     public FlightSlippy(){
         super("flight_slippy", 4, 0);
     }
+    
     public void act() 
     {
         super.act();
-    }    
+        moveVertically();
+        moveHorizontally();
+        updateOrientation();
+    }
+    
+    public void updateOrientation(){
+        if((Greenfoot.isKeyDown("left") && facingForwards) 
+            || (Greenfoot.isKeyDown("right") && !facingForwards)){
+            reverseOrientation();
+        }
+    }
+    
+    public void reverseOrientation(){
+        facingForwards = !facingForwards;
+        mirrorAll();
+    }
+    
+    private void moveHorizontally() {
+        
+        Actor left = getOneObjectAtOffset(-13, 0, Actor.class);
+        Actor right = getOneObjectAtOffset(13, 0, Actor.class);
+        
+        if (Greenfoot.isKeyDown("right"))
+        {
+            if (xVelocity < 0)
+            {
+                xVelocity = 0;
+            }
+            if (xVelocity < maxXVelocity)
+            {
+                xVelocity += 1;
+            }
+            if (right != null) {
+                xVelocity = 0;
+            }
+            
+        }
+        
+        if (Greenfoot.isKeyDown("left"))
+        {
+            if (xVelocity > 0)
+            {
+                xVelocity = 0;
+            }
+            if (xVelocity > -maxXVelocity)
+            {
+                xVelocity -= 1;
+            }
+            if (left != null) {
+                xVelocity = 0;
+            }
+            
+        }
+       
+        if (!Greenfoot.isKeyDown("left") && !Greenfoot.isKeyDown("right"))
+        {
+            xVelocity = 0;
+        }
+
+        setLocation(getX() + (int)xVelocity, getY());
+    }
+    
+    private void moveVertically() {
+        
+        Actor below = getOneObjectAtOffset(-5, 
+        (getImage().getHeight()/2 - 2) + (int)yVelocity, Actor.class);
+        Actor above = getOneObjectAtOffset(-5, 
+        -30 , Actor.class);
+        
+        if(Greenfoot.isKeyDown("up") && above == null){
+            setLocation(getX(), getY() - 5);
+        } else if(Greenfoot.isKeyDown("down")){
+            setLocation(getX(), getY() + 5);
+        } else {
+            setLocation(getX(), getY() + 1);
+        }
+       
+        
+        
+    }
 }
